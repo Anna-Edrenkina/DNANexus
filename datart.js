d077e46190bac0d9ecff46b5f6521c6a7549c64a
Anna-Edrenkina <100293868+Anna-Edrenkina@users.noreply.github.com>
2022-02-23 15:25:29 +0100

    Update 1
    Deleted alza test
    Updated datart test

--- datart.js
+++ datart.js
@@ -1,10 +1,7 @@
-
-
 /// <reference types="cypress" />
 
+// I hade an error "ufo is not founded" when i tried to get to the TV category, but probably it was not related with the bug in cypress test, but with the bug in HTML code on datart website"
 Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
   return false
 })
 
@@ -17,25 +14,27 @@ describe('Datart test', () => {
       cy.contains('Souhlasím').click()
 
       // Choose the first category "Televize, audio a video" in main cataloge.
-      // cy.get('.main-menu-catalog-list')
-      //   .get('.main-menu-catalog-category:nth-child(1)')
-      //   .click()
-
-      // 
       cy.get('.link-name').contains('Televize').click()
 
+      // Choose "Televize" category and click on it  
       cy.get('.category-tree-title').contains('Televize').click()
 
+      // Filter the TV list by price, where the most expensive are at the top
       cy.get('.sort-panel-select').select(3).should('have.value', '4')
 
+      // Wait until TV list is visible
       cy.url().should('contain', '/filter')
-      
+
+      // Choose the first TV in the list, click on button "Vlozit do kosiku".
       cy.get('.item-cart').eq(0).click()
-      
+
+      // Wait until close button is visible and click on it.
       cy.get('.close').should('be.visible').click()
-      
+
+      // Choose the second TV in the list, click on button "Vlozit do kosiku"
       cy.get('.item-cart').eq(1).click()
-      
+
+      // Wait until close button is visible and click on it.
       cy.get('.close').should('be.visible').click()
               
     
